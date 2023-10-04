ef4ba2aede6354d3d5543eb8f5f96718f3f069e0
Yaman Sigdel <Yamansigdel999@gmail.com>
2023-10-04 12:19:07 +0545

    regular_exp1

---
+++ Revise/regular_exp1.py
@@ -0,0 +1,13 @@
+#regular expression 
+import re
+
+message='My phone number is 98-40031339 & 98-48430749'
+
+#for single data extraction
+phoneobj=re.compile(r'\d\d-\d\d\d\d\d\d\d\d')
+find=phoneobj.search(message)
+print(find.group())
+
+#for all data extraction
+print(phoneobj.findall(message))
+
