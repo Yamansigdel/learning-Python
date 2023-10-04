af1e5a543f813b29f49d75c8189863c61920d1ca
Yaman Sigdel <Yamansigdel999@gmail.com>
2023-10-04 11:47:24 +0545

    Changeisdone

--- Revise/key_bind.py
+++ Revise/key_bind.py
@@ -7,8 +7,16 @@ def leftkey(event):
 def Rightkey(event):
     print('Right Key pressed')
 
+def Upkey(event):
+    print('Up Key pressed')
+
+def Downkey(event):
+    print('Down Key pressed')
+
 main.bind('<Left>',leftkey)
 main.bind('<Right>',Rightkey)
+main.bind('<Up>',Upkey)
+main.bind('<Down>',Downkey)
 
 main.mainloop()
 
