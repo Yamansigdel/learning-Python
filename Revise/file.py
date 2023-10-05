fad9903f54141f0b49f011fed862ce29d643aa00
Yaman Sigdel <Yamansigdel999@gmail.com>
2023-10-05 09:28:42 +0545

    shelve file

--- Revise/file.py
+++ Revise/file.py
@@ -1,3 +1,24 @@
-import os
 
-os.getcwd()
+# import file
+# file=open('d:/Python Programming/My Works/Revise/file.txt','w')
+# file.write('This is a practice session \n')
+# file.close()
+# file=open('d:/Python Programming/My Works/Revise/file.txt','a')
+# file.write('Appending the file')
+# file.close()
+
+# file=open('d:/Python Programming/My Works/Revise/file.txt','r')
+# file.read()
+# file.close()
+
+import shelve
+
+# shelfile=shelve.open('mydata')
+# shelfile['cats']=['zombie','hina','sina','oren']
+# shelfile.close()
+# shelfile=shelve.open('mydata')
+# shelfile['cats']
+# list(shelfile.keys())
+
+
+
