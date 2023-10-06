176c7ee2cab4ffd0d371dd482b3d6eea48d82301
Yaman Sigdel <Yamansigdel999@gmail.com>
2023-10-06 20:05:27 +0545

    gui

--- Revise/gui.py
+++ Revise/gui.py
@@ -1,17 +1,18 @@
 import pyautogui
 
-width,heigth=pyautogui.size()
+# width,heigth=pyautogui.size()
 
-print(width, heigth)
-print(pyautogui.position())
-pyautogui.moveTo(810,540,1.5)
-pyautogui.moveRel(200,0)
+# print(width, heigth)
+# print(pyautogui.position())
+# pyautogui.moveTo(810,540,1.5)
+# pyautogui.moveRel(200,0)
 
 
-# pyautogui.click(100,100);pyautogui.typewrite('Hello World !!', interval=0.2)
-pyautogui.click(100,100);pyautogui.typewrite(['a','b','left','left','X','Y'], interval=0.2)
-pyautogui.press('f1')
+# # pyautogui.click(100,100);pyautogui.typewrite('Hello World !!', interval=0.2)
+# pyautogui.click(100,100);pyautogui.typewrite(['a','b','left','left','X','Y'], interval=0.2)
+# pyautogui.press('f1')
 
-pyautogui.hotkey('win','r')
+# pyautogui.hotkey('win','r')
 
+pyautogui.screenshot('d:/screenshot_example.jpg')
 
